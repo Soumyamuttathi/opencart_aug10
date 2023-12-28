c77bb52c615086878a9712145efd05b8684cac6f
soumya <soumyamuttathi12@gmail.com>
2023-12-28 20:09:03 +0530

    commiting project

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\soumy\eclipse-workspace\opencart_aug10
+mvn test
---
+++ src/test/java/pageObjects/SearchPage.java
@@ -0,0 +1,85 @@
+package pageObjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchPage extends BasePage
+{   
+	WebDriver driver;
+	
+	public SearchPage(WebDriver driver)
+	{
+		super(driver);
+	}	
+	
+		@FindBy(xpath="//header/div[1]/div[1]/div[2]/div[1]/input[1]")
+		List<WebElement> searchProducts;
+		
+		@FindBy(xpath="//h4[1]/a[1]")
+		WebElement txtProduct;
+				
+		@FindBy(xpath="//input[@id='input-quantity']")
+		WebElement txtquantity;
+		
+		@FindBy(xpath="//button[@id='button-cart']")
+		WebElement btnaddToCart;
+		
+		@FindBy(xpath="//div[@id='alert']")
+		WebElement cnfMsg;
+		
+		public boolean isProductExist(String productName)
+		{
+			boolean flag=false;
+			for(WebElement product:searchProducts)
+			{				
+				if(product.getAttribute("title").equals(productName))
+				{
+				flag=true;
+				break;
+				}
+			}
+			
+			return flag;
+		
+		}
+		
+		public void selectProduct(String productName)
+		{
+			for(WebElement product:searchProducts)
+			{				
+				if(product.getAttribute("title").equals(productName))
+				{
+					product.click();
+				}
+			}
+		
+		}
+		
+		public void setQuantity(String qty)
+		{
+			txtquantity.clear();
+			txtquantity.sendKeys(qty);
+		}
+		
+		public void addToCart()
+		{
+			btnaddToCart.click();
+		}
+		
+		public boolean checkConfMsg()
+		{
+			try
+			{
+			return cnfMsg.isDisplayed();
+			}
+			catch(Exception e)
+			{
+				return false;
+			}
+		}
+		
+}
+
